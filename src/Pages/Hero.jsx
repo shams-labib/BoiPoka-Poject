fc352c95bd1629c75a0bf8245fdc59509a0cae08
shams <shamsallabib@gmail.com>
2025-10-03 04:31:39 +0600

    added hero section

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -1,27 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router';
+import BigBooks from '../assets/pngwing 1.png'
 
 const Hero = () => {
     return (
-       <div
-  className="hero min-h-screen"
-  style={{
-    backgroundImage:
-      "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
-  }}
->
-  <div className="hero-overlay"></div>
-  <div className="hero-content text-neutral-content text-center">
-    <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Welcome To The Books World</h1>
-      <p className="mb-5">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
-      <Link to={'/home'} ><button className="btn btn-primary">Get Started</button></Link>
-    </div>
-  </div>
-</div>
+     <div className='container mx-auto  border-1 border-gray-400 rounded-2xl shadow-xl bg-[#7CC4F8] mt-5 mb-5 grid grid-cols-2 place-items-center lg:px-[150px] py-[50px]  '>
+        <div>
+            <h1 className='text-[56px] font-bold'>Books to freshen up your bookshelf</h1>
+            <button className='bg-green-600 rounded-xl font-bold py-[16px] px-[28px]'>View The List</button>
+        </div>
+        <div>
+          <img className='h-full' src={BigBooks} alt="" />
+        </div>
+     </div>
     );
 };
 
