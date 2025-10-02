a1e7c60f31e2abcb9aa335daf7f5f85d3af6672f
shams <shamsallabib@gmail.com>
2025-10-03 01:58:45 +0600

    added

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Footer from './Pages/Footer'
+import Hero from './Pages/Hero'
+import Navber from './Pages/Navber'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Navber></Navber>
+    <Hero></Hero>
+    <Footer></Footer>
+    </>
+  )
+}
+
+export default App
